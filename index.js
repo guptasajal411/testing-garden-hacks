66c631dd0f4db8ea451144305897ef46107e117f
Sajal Gupta <70312106+guptasajal411@users.noreply.github.com>
2021-06-19 15:20:00 +0530

    Remove location error by the API

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     
-    // Get Location 
-    navigator.geolocation.getCurrentPosition(success, error);
+        navigator.geolocation.getCurrentPosition(success, error);
 
     function success(pos) {
         var lat = pos.coords.latitude;
@@ -14,13 +13,29 @@ $(document).ready(function () {
         $("#city").html("Allow location permission to proceed.");
     }
 
-    // Call Weather
     function weather(lat, long) {
         var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
         $.getJSON(URL, function(data) {
             updateDOM(data);
             console.log(typeof data.main.temp);
+        });
+    }
+    function updateDOM(data) {
+        if(data.name == "Shuzenji"){
+            $("#temperature").html("");
+            $("#humidity").html("");
+            $("#city").html("");
+            $("#wind").html("");
+            $("#prescription1").html("");
+            $("#prescription1").html("Loading, please wait...");
+            location.reload();
+        }
+        else{
+            $("#temperature").html(data.main.temp_max + "° Celcius");
+            $("#humidity").html("Humidity is: " + data.main.humidity + "%");
+            $("#city").html("Your city: " + data.name);
+            $("#wind").html("Wind speed: " + data.wind.speed + "kmph");
             if (data.main.temp_max > "25"){
                 console.log("u got bamboozled")
                 $("#prescription").html("The temperature is suitable for <em>kharif</em> crops");
@@ -36,20 +51,6 @@ $(document).ready(function () {
             else{
                 $("#prescription1").html("It is raining, go save your crops!");
             }
-            if(data.name == "Shuzenji"){
-                $("#city").html("At your location:");
-            }
-        });
+        }
     }
-    function updateDOM(data) {
-        $("#temperature").html(data.main.temp_max + "° Celcius");
-        $("#humidity").html("Humidity is: " + data.main.humidity + "%");
-        $("#city").html("Your city: " + data.name);
-        $("#wind").html("Wind speed: " + data.wind.speed + "kmph");
-        // console.log(data);
-        // console.log("update dom");
-        // console.log(data.name);
-        // console.log(data.main.temp_max);
-    }
-
 })
